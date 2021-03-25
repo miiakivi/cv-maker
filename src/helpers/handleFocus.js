aa972cc39ea55346225929e0418bf75d789a42da
MiiaNyy <mi.nyyssonen@gmail.com>
2021-03-25 12:04:39 +0200

    updated accessibilaty and added more code splitting

--- src/helpers/handleFocus.js
+++
@@ -1,8 +0,0 @@
-// When user starts editing any section, when input field is focused,
-// all of the text is selected
-
-function handleFocus (e) {
-    e.target.select();
-}
-
-export default handleFocus
--- src/helpers/openItemEditingForm.js
+++
@@ -1,9 +0,0 @@
-import { updateState } from "./updateState";
-
-function openItemEditingForm(obj, stateUpdater) {
-    let newState = obj;
-    newState.editMode = true;
-    updateState(stateUpdater, newState.id, newState)
-}
-
-export default openItemEditingForm;
